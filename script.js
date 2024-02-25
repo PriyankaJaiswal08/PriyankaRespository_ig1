67207b41628d61f23fb4b68872d2e06a292dea59
PriyankaJaiswal08 <156229795+PriyankaJaiswal08@users.noreply.github.com>
2024-02-25 11:55:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var istatus= document.querySelector("h5")
+var btn= document.querySelector("#add")
+var check=0
+
+btn.addEventListener("click", function(){
+    if (check==0){
+    istatus.innerHTML= "Friends"
+    istatus.style.color="green"
+    btn.innerHTML="Remove Friend"
+    check=1
+
+    }else {
+        btn.innerHTML="Add Friend"
+        istatus.innerHTML= "Strangers"
+        istatus.style.color="red"  
+        check=0
+    }
+})
+
